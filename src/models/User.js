b03f3e9efacb578da1871a41d23e67247a292faa
Eduardo <franciscoeduardo278@gmail.com>
2021-12-08 16:02:03 -0300

    cria servidor, configura base de dados e cria o primeiro model

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User
