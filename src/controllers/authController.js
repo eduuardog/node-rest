55ced1404da55c4f8226cd5dcd964a45fcf42d3f
Eduardo <franciscoeduardo278@gmail.com>
2021-12-10 10:32:22 -0300

    adiciona nodemon

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const router = express.Router();
@@ -18,6 +19,26 @@ router.post('/register', async (request, response) => {
   } catch (error) {
     return response.status(400).send({ error: 'Registration failed' });
   }
+})
+
+router.post('/authenticate', async (request, response) => {
+
+  const { email, password } = request.body;
+  try {
+    const user = await User.findOne({ email }).select('+password');
+    if (!user) return response.status(400).send({ error: 'User not found' });
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) return response.status(400).send({ error: 'Invalid password' });
+
+    user.password = undefined;
+
+    return response.json({ user });
+  } catch (error) {
+    return response.status(500).send(error.message);
+  }
+
+
 })
 
 module.exports = app => app.use('/auth', router);
